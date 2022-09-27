3fb8720371dc8356803284a6fe1810b40cbd2304
Andraoz85 <andraoz85@gmail.com>
2022-09-27 11:16:57 +0200

    Create Customer.cs
    added customer

---
+++ Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Onlinerestaurang
+{
+    class Customer : Users
+    {
+        private string customerName;
+        private string customerAdress;
+        private string phoneNumber;
+        private string eMail;
+
+        public Customer(string customerName, string customerAdress, string phoneNumber, string eMail)
+        {
+            this.customerName = customerName;
+            this.customerAdress = customerAdress;
+            this.phoneNumber = phoneNumber;
+            this.eMail = eMail;
+        }
+
+        public void AddoCart(string meal)
+        {
+            Console.WriteLine("You added " + meal);
+        }
+
+
+    }
+
+
+}
