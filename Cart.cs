391c4f0812143fff86b91f35e59b8a6109be25c6
Andraoz85 <andraoz85@gmail.com>
2022-09-27 11:15:47 +0200

    Create Cart.cs
    added cart

---
+++ Cart.cs
@@ -0,0 +1,28 @@
+﻿namespace Onlinerestaurang
+{
+    class Cart
+    {
+        private string foodType;
+        private int foodPrice;
+
+        public Cart(string foodType, int foodPrice)
+        {
+            this.foodType = foodType;
+            this.foodPrice = foodPrice;
+        }
+
+        public void addToCart(string foodType, int foodPrice)
+        {
+            this.foodType = foodType;
+            this.foodPrice = foodPrice;
+        }
+
+        public void ProceedToPayment()
+        {
+
+        }
+
+
+
+    }
+}
