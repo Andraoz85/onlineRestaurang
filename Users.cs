de745c16cdf9e9be1f3f27bb90185f172c97e0cd
Andraoz85 <andraoz85@gmail.com>
2022-09-27 11:17:31 +0200

    Create Users.cs
    added users

---
+++ Users.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onlinerestaurang
+{
+    class Users
+    {
+        protected string userName { get; set; }
+        private string password { get; set; }
+
+        public void Login()
+        {
+        }
+
+        public void Register()
+        {
+        }
+
+        public void ViewMenu(Menu menu)
+        {
+            Console.WriteLine("andy spel");
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
