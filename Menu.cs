38edfda99396bf4f75fcac4af54f35cdf732cc63
Andraoz85 <andraoz85@gmail.com>
2022-09-27 11:17:05 +0200

    Create Menu.cs
    add menu

---
+++ Menu.cs
@@ -0,0 +1,38 @@
+﻿namespace Onlinerestaurang
+{
+    class Menu
+    {
+        private string foodType;
+        private int foodPrice;
+
+
+        public Menu(string foodType, int foodPrice)
+        {
+            this.foodType = foodType;
+            this.foodPrice = foodPrice;
+        }
+
+        public void UpdateFoodType()
+        {
+
+        }
+
+        public void UpdatePrice()
+        {
+
+        }
+        public string ShowMenu()
+        {
+            return foodType + foodPrice;
+        }
+        public string GetFoodType()
+        {
+            return foodType;
+        }
+        public int GetFoodPrice()
+        {
+            return foodPrice;
+        }
+    }
+}
+
